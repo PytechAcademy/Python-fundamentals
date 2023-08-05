309dcb1cac61a5dd5ea4ca9263eeb5f0895fe37c
rajaauditone <109960357+rajaauditone@users.noreply.github.com>
2023-08-05 10:01:05 +0200

    Update 3.VariablesScope.py

--- 06.Functions/3.VariablesScope.py
+++ 06.Functions/3.VariablesScope.py
@@ -1,21 +1,14 @@
 def example_function():
     x = 10  # Local variable
     print(x)
-
-
 example_function()
 # print(x)  # This will raise an error as x is not defined outside the function.
-
 c = 10  # Global variable
-
 
 def example_function():
     print(c)
-
-
 example_function()
 print(c)  # Global variable c can be accessed outside the function.
-
 
 def outer_function():
     x = 10
